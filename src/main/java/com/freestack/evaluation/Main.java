12f5ba5e3f0d172b41cdd00b6feee95adb8bed4a
Lea-Vangheluwe <lv.vangheluwe@gmail.com>
2021-12-03 10:40:19 +0100

    dde

--- src/main/java/com/freestack/evaluation/Main.java
+++ src/main/java/com/freestack/evaluation/Main.java
@@ -11,6 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //commentaire test
         UberUser uberUser = new UberUser("joe", "bah");
         UberApi.enrollUser(uberUser);
 
